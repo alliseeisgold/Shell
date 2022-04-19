a9b348dd1239af3e94e8fcbdf97e97254241c0fd
Asror Mirzoev <coder2002a@gmail.com>
2022-04-19 18:21:15 +0300

    Added bonus part: run executable files

---
+++ src/Bonus_Func.py
@@ -0,0 +1,28 @@
+# Bonus Functionality
+import subprocess
+import os
+
+
+class Bonus:
+    """
+        Здесь находится реализация бонусной части.
+        Кроме тестов. Тесты находятся в папке tests в tests.py
+    """
+    @staticmethod
+    def exec_files_runner(args: list):
+        """
+            Сначала проверяем существует ли заданный файл и потом проверяем
+            является ли исполняемым. Если оба условия верны, то запускаем
+            исполняемый файл с заданными аргументами.
+        """
+        if os.path.exists(args[0]):
+            if os.access(args[0], os.X_OK):
+                subprocess.run(args)
+            else:
+                print('This file isn\'t executable.')
+        else:
+            print('There isn\'t such file.')
+
+
+
+
