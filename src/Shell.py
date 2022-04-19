7fa02a8b48c377f8c1d849baf79f71b9f4449479
Asror Mirzoev <coder2002a@gmail.com>
2022-04-19 18:19:25 +0300

    Changed Shell.py

--- src/Shell.py
+++ src/Shell.py
@@ -1,29 +1,25 @@
-# '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'
 import os
-import socket
 from src.Basic_Func import Basic
 from src.Bonus_Func import Bonus
+from src.User import User
 from termcolor import colored
 
 
 class Shell:
 
-    def __init__(self):
-        self.username = os.getlogin()
-        self.hostname = socket.gethostname()
-
-    def getUserName(self):
-        return self.username
-
-    def getUser(self):
-        user = self.username + '@' + self.hostname + ':~'
-        return user
-
     @staticmethod
-    def check_for_len(a: list, n: int):
+    def print_extra_files(a: list, n: int):
+        """
+            Если в команде есть лишние аргументы, то печатает их и игнорирует.
+            Это функция предназначена для избежания дубликатов.
+        """
         print('{0}: there extra operand(s) {1}. They(It) will be ignored.'.
               format(colored(a[0], attrs=['bold']),
                      colored(str([a[idx] for idx in range(n, len(a))]), 'yellow', attrs=['bold'])))
+
+    @staticmethod
+    def print_not_enough(s: str):
+        print('{0}: the operand specifying the file is omitted'.format(colored(s, attrs=['bold'])))
 
     @staticmethod
     def parser_runner(args: str):
@@ -47,17 +43,33 @@ class Shell:
         """
         basic = Basic()
         bonus = Bonus()
-        b = args.split(' ')
+        b = args.split()
         a = []
-        for items in range(len(b)):
-            if not b[items] == '':
-                a.append(b[items])
+        i = 0
+        while i < len(b):
+            if b[i] != '':
+                if b[i][0] == "'":
+                    s = b[i][1:]
+                    if b[i][-1] == "'":
+                        s = b[i][1:len(b[i]) - 1]
+                    else:
+                        s += ' '
+                        i += 1
+                        while i < len(b) and b[i][-1] != "'":
+                            s += b[i] + ' '
+                            i += 1
+                        if i < len(b):
+                            s += b[i][0:len(b[i]) - 1]
+                    a.append(s)
+                else:
+                    a.append(b[i])
+            i += 1
         if a[0] == 'run':
             a[1] = os.path.abspath(a[1])
             bonus.exec_files_runner(a[1:])
         elif a[0] == 'help' or a[0] == 'ls' or a[0] == 'pwd' or a[0] == 'clear' or a[0] == 'exit':
             if len(a) > 1:
-                Shell.check_for_len(a, 1)
+                Shell.print_extra_files(a, 1)
             if a[0] == 'ls':
                 basic.ls()
             elif a[0] == 'pwd':
@@ -70,10 +82,10 @@ class Shell:
                 os.system('clear')
         elif a[0] == 'cd' or a[0] == 'rm' or a[0] == 'rmdir' or a[0] == 'mkdir':
             if len(a) < 2:
-                print('{0}: the operand specifying the file is omitted'.format(colored(a[0], attrs=['bold'])))
+                Shell.print_not_enough(a[0])
             else:
                 if len(a) > 2:
-                    Shell.check_for_len(a, 2)
+                    Shell.print_extra_files(a, 2)
                 if a[0] == 'cd':
                     basic.cd(a[1])
                 elif a[0] == 'rm':
@@ -85,10 +97,10 @@ class Shell:
         elif a[0] == 'cp' or a[0] == 'mv':
             print(11)
             if len(a) < 3:
-                print('{0}: the operand specifying the file is omitted'.format(colored(a[0], attrs=['bold'])))
+                Shell.print_not_enough(a[0])
             else:
                 if len(a) > 3:
-                    Shell.check_for_len(a, 3)
+                    Shell.print_extra_files(a, 3)
                 if a[0] == 'cp':
                     basic.cp(a[1], a[2])
                 else:
@@ -110,18 +122,18 @@ class Shell:
 
     @staticmethod
     def run():
-        sh = Shell()
+        users = User()
         os.system('clear')
-        sh.welcome()
-        user = sh.getUser()
+        Shell.welcome()
+        user = users.getUser()
         while True:
-            print(colored(user, 'red', attrs=['bold']), end=' ')
+            print(colored(user, 'cyan', attrs=['bold']), end=' ')
             print(colored(
-                os.path.abspath(os.curdir)[len(sh.getUserName()) + 6:] + '$',
+                '~' + os.path.abspath(os.curdir)[len(users.getUserName()) + 6:] + '$',
                 'blue',
                 attrs=['bold']),
                 end=' ')
             cmd = input()
-            if sh.parser_runner(cmd) == 1:
+            if Shell.parser_runner(cmd) == 1:
                 os.system('clear')
                 break
